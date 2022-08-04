ac426ce795e9768d3e28b98333e997b0ba9c1809
«Dmitrii <Ambal003@ya.ry>
2022-08-04 22:34:15 +0300

    Сделал 1е задание

---
+++ Task#1/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] GetArray(int size, int min, int max)
+{
+    int[] result = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(min, max + 1);
+    }
+
+    return result;
+}
+
+Console.WriteLine("Введите длинну массива");
+int length = int.Parse(Console.ReadLine());
+int[] array = GetArray(length, 100, 999);
+int n = 0;
+for (int i = 0; i < length; i++)
+{
+  Console.Write(array[i] + " ");
+}
+  foreach (int i in array)
+  {
+    if (i % 2 == 0)n++;
+  }
+  Console.WriteLine();
+  Console.WriteLine("Количество четных чисел в массиве = " + n);
